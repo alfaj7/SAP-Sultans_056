4fe260720783fea6295839122ff98fa8c4736dd5
suraj0119 <surajkamble1319@gmail.com>
2024-06-14 02:36:56 +0530

    ft33_152_day3 API added succesfully

--- userPannel/script.js
+++ userPannel/script.js
@@ -2,22 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const musicList = document.getElementById('music-list');
     const audioPlayer = document.getElementById('audio-player');
 
-    fetch('db.json')
+    fetch('https://mock-api-template-wfxc.onrender.com/songs')
         .then(response => response.json())
         .then(data => {
-            data.songs.forEach(song => {
+            data.forEach(song => {
                 const songDiv = document.createElement('div');
                 songDiv.classList.add('song');
                 songDiv.innerHTML = `
-                    <img src="${song.albumArt}" alt="Album Art">
+                    <img src="${song.songposterurl}" alt="Album Art">
                     <div class="song-info">
-                        <h3>${song.title}</h3>
+                        <h3>${song.name}</h3>
                         <p>${song.artist}</p>
-                        <p>${song.duration}</p>
+                        <p>${song.Duration}</p>
                     </div>
                 `;
                 songDiv.addEventListener('click', () => {
-                    audioPlayer.src = song.url;
+                    audioPlayer.src = song.songurl;
                     audioPlayer.play();
                     audioPlayer.style.display = 'block';
                 });
